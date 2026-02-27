b0de5e355dbf4bf96afdda1feacc2b73aec28b79
Emong <40270979+MaoEmong@users.noreply.github.com>
2026-02-27 14:07:54 +0900

    init

---
+++ src/main/java/com/example/travel_platform/TravelPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.travel_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelPlatformApplication.class, args);
+	}
+
+}
