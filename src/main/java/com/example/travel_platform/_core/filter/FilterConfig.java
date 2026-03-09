5406474d685c042c6ee4273f950885c2ffa8e2a2
Emong <40270979+MaoEmong@users.noreply.github.com>
2026-03-09 17:35:49 +0900

    중간 세이브

--- src/main/java/com/example/travel_platform/_core/filter/FilterConfig.java
+++ src/main/java/com/example/travel_platform/_core/filter/FilterConfig.java
@@ -12,7 +12,7 @@ public class FilterConfig {
         FilterRegistrationBean<LoginFilter> bean = new FilterRegistrationBean<>();
         bean.setFilter(new LoginFilter());
         bean.addUrlPatterns("/boards/*"); // /boards/ 로 시작하는 모든 요청에 대해 검사
-        bean.addUrlPatterns("/replies/*"); // /replies/ 로 시작하는 모든 요청에 대해 검사
+        // bean.addUrlPatterns("/replies/*"); // /replies/ 로 시작하는 모든 요청에 대해 검사
         bean.setOrder(2); // 필터 순서
         return bean;
     }
