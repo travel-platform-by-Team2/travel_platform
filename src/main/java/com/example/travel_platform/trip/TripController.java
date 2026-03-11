ffb3bf07cfe006b7f70ed6c0b134d1a88cc7dd85
KHJ <40270979+MaoEmong@users.noreply.github.com>
2026-03-11 14:43:32 +0900

    feat: 로그인 상태일때 헤더에 판매자센터 버튼 추가 , trip api주소 간결화
    * chore: 엔티티 정의서, 테이블 정의서 생성/RULES문서 수정
    
    * AI 가이드 문서 작성 진행 중
    
    * ai agent 문서 작성
    
    * 충돌 수정
    
    * 프로젝트 전체적인 리팩토링 진행
    
    * 챗봇 중간 세이브
    
    * feat: 지도 api키값 env로 옮김
    
    * docs폴더 깃 등록 그만
    
    * 중간 세이브
    
    * feat: logout header 로그인페이지 리다이렉트 처리
    
    * 헤더 반응처리 완료
    
    * 작업 관련 데이터들의 CONTEXT 문서 생성
    
    * fix: 여행계획 템플릿 api주소 명  수정
    
    * fix: 챗봇 기능 수정
    
    * feat: 로그인 상태일때 헤더에 판매자센터 버튼 추가 , trip api주소 간결화

--- src/main/java/com/example/travel_platform/trip/TripController.java
+++ src/main/java/com/example/travel_platform/trip/TripController.java
@@ -4,29 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/trip")
 @Controller
 public class TripController {
 
-    @GetMapping("/trip-list")
+    @GetMapping()
     public String tripListPage() {
         return "pages/trip-list";
     }
 
-    @GetMapping("/trip-create")
+    @GetMapping("/create")
     public String tripCreatePage() {
         return "pages/trip-create";
     }
 
-    @GetMapping("/trip-detail/{planId}")
-    public String tripDetailPage(@PathVariable Integer planId, Model model) {
-        model.addAttribute("planId", planId);
+    @GetMapping("/detail")
+    public String tripDetailPage() {
         return "pages/trip-detail";
     }
 
-    @GetMapping("/trip-add-place/{planId}")
-    public String tripAddPlacePage(@PathVariable Integer planId, Model model) {
-        model.addAttribute("planId", planId);
+    @GetMapping("/place")
+    public String tripAddPlacePage() {
         return "pages/trip-add-place";
     }
 }
