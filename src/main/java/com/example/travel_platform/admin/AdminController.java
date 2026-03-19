534af212be0b713854ade4ff3c2de9afabf84fb1
Emong <40270979+MaoEmong@users.noreply.github.com>
2026-03-19 10:07:22 +0900

    prtest

--- src/main/java/com/example/travel_platform/admin/AdminController.java
+++ src/main/java/com/example/travel_platform/admin/AdminController.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 public class AdminController {
     private final AdminService adminService;
 
+    // prTest
     @GetMapping("")
     public String dashboard(Model model) {
         applySidebarState(model, "dashboard");
